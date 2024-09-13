676d13066ad2049ed920ce86ac49a82aba0fb5e9
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-13 22:40:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+let container = document.querySelector("#code-container");
+for(let i = 0; i < 6; i++){
+	let field = document.createElement("input");
+	field.className = "code";
+	field.type = "number";
+	field.name = "otp";
+	container.appendChild(field);
+}
+
+let inputfields = document.querySelectorAll(".code");
+for(let i = 0; i < inputfields.length; i++){
+	inputfields[i].addEventListener('input', function(event) {
+		// Check if the input field contains a number
+		if (inputfields[i].value !== "" && i+1 < inputfields.length) {
+			// Focus on the next input field
+			inputfields[i+1].focus();
+		}
+	});
+	
+	inputfields[i].addEventListener('input', function(event) {
+		// Check if the backspace key was pressed and the input field is empty
+		if (inputfields[i].value === "" && i-1 >= 0) {
+			// Focus on the previous input field
+			inputfields[i-1].focus();
+		}
+	});
+}

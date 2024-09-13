b40d45e8bd4bc99dd07029eb0d749a94f96f526c
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-13 22:47:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ for(let i = 0; i < 6; i++){
 
 let inputfields = document.querySelectorAll(".code");
 for(let i = 0; i < inputfields.length; i++){
-	inputfields[i].addEventListener('input', function(event) {
+	inputfields[i].addEventListener('input', function() {
 		// Check if the input field contains a number
 		if (inputfields[i].value !== "" && i+1 < inputfields.length) {
 			// Focus on the next input field
@@ -17,7 +17,7 @@ for(let i = 0; i < inputfields.length; i++){
 		}
 	});
 	
-	inputfields[i].addEventListener('input', function(event) {
+	inputfields[i].addEventListener('input', function() {
 		// Check if the backspace key was pressed and the input field is empty
 		if (inputfields[i].value === "" && i-1 >= 0) {
 			// Focus on the previous input field

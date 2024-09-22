011399802a0dd7f1d2013e61162ed2a2405dcfc5
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-22 19:32:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 let container = document.querySelector(".code-container");
 for(let i = 0; i < 6; i++){
 	let field = document.createElement("input");
-	field.id = "code";
+	field.className = "code";
 	field.type = "number";
 	field.name = "otp";
 	container.appendChild(field);
 }
 
-let inputfields = document.querySelectorAll("#code");
+let inputfields = document.querySelectorAll(".code");
 for(let i = 0; i < inputfields.length; i++){
 	inputfields[i].addEventListener('input', function() {
 		// Check if the input field contains a number
 		if (inputfields[i].value !== "" && i+1 < inputfields.length) {
 			// Focus on the next input field
-			inputfields[i+1].focus();
+			inputfields[i+1].classList.add('focused');
+			inputfields[i].classList.remove('focused');
+			
 		}
 	});
 	
@@ -21,7 +23,9 @@ for(let i = 0; i < inputfields.length; i++){
 		// Check if the backspace key was pressed and the input field is empty
 		if (inputfields[i].value === "" && i-1 >= 0) {
 			// Focus on the previous input field
-			inputfields[i-1].focus();
+			inputfields[i-1].classList.add("focused")
+			inputfields[i+1].classList.remove("focused")
+			
 		}
 	});
 }
